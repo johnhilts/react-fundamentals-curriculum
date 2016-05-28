ddde69f866455bfd4e515492bb14bad51a1175ef
John Hilts <johnhilts@live.com>
2016-05-28 11:00:48 -0700

    Step 2
    added main container

--- app/config/routes.js
+++ app/config/routes.js
@@ -4,11 +4,11 @@ var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var hashHistory = ReactRouter.hashHistory;
 var IndexRoute = ReactRouter.IndexRoute;
-var Home = require("../components/Home");
+var Main = require("../containers/Main");
 
 var routes = (
   <Router history={hashHistory}>
-    <Route path='/' component={Home}>
+    <Route path='/' component={Main}>
     </Route>
   </Router>
 );
--- app/containers/Main.js
+++ app/containers/Main.js
@@ -2,14 +2,16 @@ var React = require('react');
 var ReactRouter = require('react-router');
 var Link = ReactRouter.Link
 
-function Home () {
+function Main () {
   return (
     <div>
       <h1>Weather App!</h1>
-      <p className='lead'>React Fundamentals Cirruculum</p>
+      <p className='lead'>React Fundamentals Cirriculum</p>
       <p>Modified by Mr John Hilts</p>
+      <p>This is now the Main container</p>
     </div>
   )
 }
 
-module.exports = Home;
+module.exports = Main;
+
