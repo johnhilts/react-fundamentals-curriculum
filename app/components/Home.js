4aed1571d03e08652b1f6c0c24868e2f883c15b6
John Hilts <johnhilts@live.com>
2016-05-28 11:30:32 -0700

    Step 3
    added basic home component
    home is now contained within Main ... because of how it's setup in
    routing?!?

---
+++ app/components/Home.js
@@ -0,0 +1,12 @@
+var React = require('react');
+var Main = require("../containers/Main");
+
+function Home (props) {
+  return (
+    <div>This is the home component</div>
+
+  )
+}
+
+module.exports = Home;
+
--- app/containers/Main.js
+++ app/containers/Main.js
@@ -2,16 +2,19 @@ var React = require('react');
 var ReactRouter = require('react-router');
 var Link = ReactRouter.Link
 
-function Main () {
-  return (
-    <div>
-      <h1>Weather App!</h1>
-      <p className='lead'>React Fundamentals Cirriculum</p>
-      <p>Modified by Mr John Hilts</p>
-      <p>This is now the Main container</p>
-    </div>
-  )
-}
+var Main = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>Weather App!</h1>
+        <p className='lead'>React Fundamentals Cirriculum</p>
+        <p>Modified by Mr John Hilts</p>
+        <p>This is now the Main container</p>
+	{this.props.children}
+      </div>
+    )
+  }  
+});
 
 module.exports = Main;
 
