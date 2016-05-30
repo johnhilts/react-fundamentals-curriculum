b5f5b718ebc1408a4bc754812966950252258b85
John Hilts <johnhilts@live.com>
2016-05-30 11:38:55 -0700

    Step 3
    Refactored main container as functional stateless component

--- app/containers/Main.js
+++ app/containers/Main.js
@@ -3,23 +3,29 @@ var ReactRouter = require('react-router');
 var Link = ReactRouter.Link
 
 var styles = {
+  header: {
+    background: 'orange',
+  },
   container: {
     width: '100%',
     height: '92%'
   }
 }
 
-var Main = React.createClass({
-  render: function() {
+function Main(props) {
     return (
-      <div style={styles.container}>
-        <h1>Main Container Header</h1>
-	{this.props.children}
-        <p>This can be the footer</p>
+      <div>
+        <div style={styles.header}>
+          <h1>Clever Header</h1>
+        </div>
+        <div style={styles.container}>
+          {props.children}
+        </div>
+        <div>
+          <p>This can be the footer</p>
+        </div>
       </div>
     )
-  }  
-});
+};
 
 module.exports = Main;
-
