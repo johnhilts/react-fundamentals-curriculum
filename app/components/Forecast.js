b2eca063f0299b71ffe7417d3383e14cb8dcb6b5
John Hilts <johnhilts@live.com>
2016-05-30 22:29:00 -0700

    Step 7
    Added handling for isLoading - show different presentation depending on
    value

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -2,12 +2,16 @@ var React = require('react');
 
 var Forecast = function(props) {
   return (
-    <div>
-      This is the forecast container!<br />
-      <br />
-      The city is: {props.city}<br />
-      The current temperature is: {props.currentTemperature}<br />
-    </div>
+    props.isLoading
+    ?
+      <div style={{background: 'red',}}>Loading ... please wait!</div>
+    :
+      <div>
+        This is the forecast container!<br />
+        <br />
+        The city is: {props.city}<br />
+        The current temperature is: {props.currentTemperature}<br />
+      </div>
   )
 };
 
